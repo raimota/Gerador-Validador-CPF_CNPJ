84b2072a040aeda5464c4338ed21db70c4536ab5
raimota <rmgithub2020@gmail.com>
2022-06-01 21:55:02 -0300

    update....

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -8,24 +8,19 @@ def index():
     form = Campos()
     if form.validate_on_submit():
         op = str(form.es.data)
-        print(form.es)
         if (len(op) == 11):
-            print('Caiu no CPF')
             result = cpf.valida_cpf(op)
             valido = 1 if result else 0
             return render_template('index.html',form=form, valido=valido, dado=op),200
         elif (len(op) > 11):
-            print('Caiu no CNPJ')
             result = cnpj.valida_cnpj(op)
             valido = 1 if result else 0
             return render_template('index.html',form=form, valido=valido, dado=op),200
         else:
-            print(f'CAiu: {request.form["op"]}')
             if request.form["op"] == 'cpf':
                 return render_template('index.html',form=form, valido=cpf.valida_cpf(cpf.gerar_cpf()),dado=cpf.gerar_cpf()),200
             else:
                 dado = cnpj.gerar_cnpj()
-                print(dado)
                 return render_template('index.html',form=form, valido=cnpj.valida_cnpj(cnpj.gerar_cnpj()), dado=dado),200
     
     return render_template('index.html',form=form),200
